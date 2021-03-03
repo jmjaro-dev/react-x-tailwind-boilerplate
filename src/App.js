ac219b843648ec7e0cefe148040de19c2b8e9d07
Jerome Mico Jaropojop <jmjaropojop92@gmail.com>
2021-03-03 15:57:55 +0800

    React+Tailwind boilerplate with dark mode.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "tailwindcss/tailwind.css";
+import './App.scss';
+import { ThemeProvider } from "./context/themeContext";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider setIsDarkMode={setIsDarkMode}>
+      <div className="App">
+      </div>
+    </ThemeProvider>
   );
 }
 
